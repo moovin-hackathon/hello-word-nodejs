45cb62c162437f0b33cf1f5e13add73f1a4fbcb3
Matheus Gonzaga <matheus.gonzaga@moovin.com.br>
2018-08-18 23:34:02 -0300

    Remove unused files

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});

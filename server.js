0edd69f3a2309958b5803da372c82c57f3e07e85
Matheus Gonzaga <matheus.gonzaga@moovin.com.br>
2018-08-18 23:05:15 -0300

    Update server port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
+const PORT = process.env.PORT || 5000
+
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
-}).listen(8080);
-console.log('Server running at http://localhost:8080/');
+}).listen(PORT);
+console.log('Server running at http://localhost:'+PORT+'/');
